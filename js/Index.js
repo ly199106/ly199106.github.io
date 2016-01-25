8e29981d242f514b9bd3f319a14a9f5366c13f27
ly199106 <xiaohaitun1991@126.com>
2016-01-25 18:54:42 +0800

    blog

--- js/Index.js
+++ js/Index.js
@@ -148,8 +148,6 @@ function generateContent() {
 /*加载单列瀑布流*/
 window.onload=function(){
 
-    //waterfall('main','pin');
-
     var dataInt={'data':[{'src':'1.jpg'},{'src':'2.jpg'}]};
     
     window.onscroll=function(){
@@ -166,38 +164,12 @@ window.onload=function(){
                 oImg.src='/image/'+dataInt.data[i].src;
                 oBox.appendChild(oImg);
             }
-            //waterfall('main','pin');
+            
         };
     }
 }
 
-/*
-    parend 父级id
-    pin 元素id
-*/
-function waterfall(parent,pin){
-    var oParent=document.getElementById(parent);// 父级对象
-    var aPin=getClassObj(oParent,pin);// 获取存储块框pin的数组aPin
-    var iPinW=aPin[0].offsetWidth;// 一个块框pin的宽
-    var num=Math.floor(document.documentElement.clientWidth/iPinW);//每行中能容纳的pin个数【窗口宽度除以一个块框宽度】
-    oParent.style.cssText='width:'+iPinW*num+'px;ma rgin:0 auto;';//设置父级居中样式：定宽+自动水平外边距
 
-    var pinHArr=[];//用于存储 每列中的所有块框相加的高度。
-    for(var i=0;i<aPin.length;i++){//遍历数组aPin的每个块框元素
-        var pinH=aPin[i].offsetHeight;
-        if(i<num){
-            pinHArr[i]=pinH; //第一行中的num个块框pin 先添加进数组pinHArr
-        }else{
-            var minH=Math.min.apply(null,pinHArr);//数组pinHArr中的最小值minH
-            var minHIndex=getminHIndex(pinHArr,minH);
-            aPin[i].style.position='absolute';//设置绝对位移
-            aPin[i].style.top=minH+'px';
-            aPin[i].style.left=aPin[minHIndex].offsetLeft+'px';
-            //数组 最小高元素的高 + 添加上的aPin[i]块框高
-            pinHArr[minHIndex]+=aPin[i].offsetHeight;//更新添加了块框后的列高
-        }
-    }
-} 
 
 /****
     *通过父级和子元素的class类 获取该同类子元素的数组
