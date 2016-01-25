a987eebabe79fd1d33b6649ed56424d31a478f10
ly199106 <xiaohaitun1991@126.com>
2016-01-25 19:01:52 +0800

    blog

--- js/Index.js
+++ js/Index.js
@@ -157,12 +157,12 @@ window.onload=function(){
                 var oPin=document.createElement('div'); //添加 元素节点
                 oPin.className='article';                   //添加 类名 name属性
                 oParent.appendChild(oPin);              //添加 子节点
-                var oBox=document.createElement('div');
-                oBox.className='box';
-                oPin.appendChild(oBox);
+               // var oBox=document.createElement('div');
+                //oBox.className='box';
+                //oPin.appendChild(oBox);
                 var oImg=document.createElement('img');
                 oImg.src='/image/'+dataInt.data[i].src;
-                oBox.appendChild(oImg);
+                oPin.appendChild(oImg);
             }
             
         };
