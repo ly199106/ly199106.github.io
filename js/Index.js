f049a2b8005a0a1332883fe01f1c4122b187faca
ly199106 <xiaohaitun1991@126.com>
2016-01-27 20:37:37 +0800

    blog

--- js/Index.js
+++ js/Index.js
@@ -92,7 +92,7 @@ function categoryDisplay() {
     $('.post-list-body>div[post-cate!=All]').hide();
     /*show category when click categories list*/
     $('.categories-list-item').click(function() {
-        var cate = $(this).attr('cate'); //get category's name
+        var cate = $(this).attr('cate'); //get tag's name
 
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
         $('.post-list-body>div[post-cate=' + cate + ']').show(400);
